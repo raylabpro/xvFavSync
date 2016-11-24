358551909c3b3eba1598cfdaeefc7de0d96c68b2
ErgoZ Vaper <ergozru@gmail.com>
2016-11-24 20:04:10 +0300

    fixes

--- configs.go
+++ configs.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"github.com/patrickmn/go-cache"
 	"net/http"
+
 	"github.com/boltdb/bolt"
+	"github.com/patrickmn/go-cache"
 )
 
 //Cache ...
 var Cache *cache.Cache
 
 type conf struct {
-	Login   string `yaml:"login"`
-	Password   string `yaml:"password"`
-	PlaylistLink   string `yaml:"playlist_link"`
-	DownloadPath   string `yaml:"download_path"`
+	Login        string `yaml:"login"`
+	Password     string `yaml:"password"`
+	PlaylistLink string `yaml:"playlist_link"`
+	DownloadPath string `yaml:"download_path"`
 }
 
 //Configs ...
@@ -26,8 +27,8 @@ var httpClient *http.Client
 var sqlDB *bolt.DB
 
 type DLInfo struct {
-	Logged bool `json:"LOGGED"`
-	Url string `json:"URL"`
+	Logged bool   `json:"LOGGED"`
+	Url    string `json:"URL"`
 	UrlLow string `json:"URL_LOW"`
 }
 
